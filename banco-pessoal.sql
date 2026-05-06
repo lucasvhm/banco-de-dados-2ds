847cd898b8c11bbe5a0605649b7c91df0838f463
lucasvhm <lucas.hubert.macedo@escola.pr.gov.br>
2026-05-06 12:30:55 -0300

    Create Funcionarios table in banco-pessoal.sql

---
+++ banco-pessoal.sql
@@ -0,0 +1,1 @@
+CREATE TABLE Funcionarios (     ID_Funcionario INT PRIMARY KEY,     Nome_Completo VARCHAR(100) NOT NULL,     Salario_Base DECIMAL(10, 2) NOT NULL,     Observacoes TEXT );  SELECT * FROM Funcionarios;
